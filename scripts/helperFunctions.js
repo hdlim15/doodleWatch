2f31e2635b2e953ea8d8cec9c167619a58fcba19
Hyuckin David Lim <hyuckin.lim15@gmail.com>
2017-08-06 02:25:20 -0400

    refactored code

--- scripts/helperFunctions.js
+++ scripts/helperFunctions.js
@@ -4,3 +4,109 @@ function cloneObject(oldObject) {
      */
     return JSON.parse(JSON.stringify(oldObject));
 }
+
+function toggleConfigDisplay(animation, clickLocation) {
+    /*
+     * Toggles the configuration display depending on where the mouse is clicked
+     */
+    var configurations = animation.getElementsByClassName("configurations")[0];
+    var colorMenu = document.getElementById("jscolorWindow");
+
+    if (animation.contains(clickLocation)) {
+        if (configurations.style.display == "block") {
+            if (!configurations.contains(clickLocation)) {
+                configurations.style.display = "none";
+            }
+        }
+        else {
+            configurations.style.display = "block";
+        }
+    }
+    else {
+        if (colorMenu && colorMenu.contains(clickLocation)) {}
+        else {
+            configurations.style.display = "none";
+        }
+    }
+}
+
+
+function deleteAnimationFromFavorites(animationCanvas) {
+    // Remove from storage
+    chrome.storage.sync.get("allFavorites", function(items) {
+        var favoriteList = items["allFavorites"]
+        for (var i = 0; i < favoriteList.length; i++) {
+            if (favoriteList[i].ID == animationCanvas.id) {
+                favoriteList.splice(i, 1);
+                chrome.storage.sync.set({allFavorites: favoriteList});
+                break;
+            }
+        }
+    });
+    // Remove canvas from DOM
+    animationCanvas.remove();
+}
+
+function addDOMCanvasToFavorites(animationType, cfg, storageID) {
+    var newCanvas = document.createElement("canvas");
+    setDOMCanvasID(newCanvas, animationType, cfg, storageID);
+    newCanvas.height = 76;
+    newCanvas.width = 76;
+
+    // append canvas to the DOM
+    document.getElementById("favorites").appendChild(newCanvas);
+
+    // add event listeners to add and delete from favorites
+    newCanvas.addEventListener("click", function() {
+        // send message to background, so icon updates even when popup is closed
+        chrome.runtime.sendMessage({
+            type: "update icon",
+            animation: animationType,
+            cfg: cfg
+        });
+    });
+    newCanvas.addEventListener("contextmenu", function(e) {
+        e.preventDefault();
+        deleteAnimationFromFavorites(this);
+    });
+
+    // Return the context of the canvas
+    var context = newCanvas.getContext("2d");
+    context.scale(4, 4);
+    return context;
+}
+
+function setDOMCanvasID(newCanvas, animationType, cfg, storageID) {
+    // if called from loadStoredFavorites
+    if (typeof storageID != "undefined") {
+        newCanvas.id = storageID;
+    }
+    // else a new animation is being added to the favorites
+    else {
+        chrome.storage.sync.get("animationID", function(items) {
+            var id = items["animationID"];
+            if (typeof id == "undefined") {
+                id = 0;
+            }
+            newCanvas.id = id;
+            chrome.storage.sync.set({animationID: id + 1});
+            addFavoriteToStorage(animationType, cfg, id);
+        });
+    }
+}
+
+function addFavoriteToStorage(animationType, cfg, animationID) {
+    var newFavoriteObject = {
+        ID: animationID,
+        animation: animationType,
+        cfg: cfg
+    };
+    chrome.storage.sync.get("allFavorites", function(items) {
+        var favoriteList = items["allFavorites"];
+        if (typeof favoriteList == "undefined") {
+            favoriteList = [];
+        }
+        favoriteList.push(newFavoriteObject);
+        chrome.storage.sync.set({allFavorites: favoriteList});
+    });
+}
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,18 +1,18 @@
 //debugging purposes
 console = chrome.extension.getBackgroundPage().console;
+// configuration dictionary that stores cfg of each animation
 var cfg_dictionary = {};
-
 
 // When the DOM is loaded
 document.addEventListener("DOMContentLoaded", function() {
     var forEach = Array.prototype.forEach;
-    var animations = document.getElementsByClassName("animation");
 
+    //--------------------- Code for the Start/Stop button ---------------------
     var startStop = document.getElementById("startStop");
-    // When popup is loaded, send a message. background will repond with isPaused
-    (function notifyPopupLoaded() {
-        chrome.runtime.sendMessage("popup loaded");
 
+    // When popup is loaded, set the correct version of the button
+    (function initializeStartStop() {
+        chrome.runtime.sendMessage("popup loaded");
         chrome.runtime.onMessage.addListener(function(message) {
             if (message == "isPaused true") {
                 startStop.style.backgroundColor = "green";
@@ -38,153 +38,18 @@ document.addEventListener("DOMContentLoaded", function() {
             chrome.runtime.sendMessage("start animation");
         }
     });
+    //^^^^^^^^^^^^^^^^^ End of code for the Start/Stop button ^^^^^^^^^^^^^^^^^^
+
 
     // Open and close the configurations window 
+    var animations = document.getElementsByClassName("animation");
     window.addEventListener("click", function(e) {
-        function toggleConfigDisplay(animation, clickLocation) {
-            var configurations = animation.getElementsByClassName("configurations")[0];
-            var colorMenu = document.getElementById("jscolorWindow");
-
-            if (animation.contains(clickLocation)) {
-                if (configurations.style.display == "block") {
-                    if (!configurations.contains(clickLocation)) {
-                        configurations.style.display = "none";
-                    }
-                }
-                else {
-                    configurations.style.display = "block";
-                }
-            }
-            else {
-                if (colorMenu && colorMenu.contains(clickLocation)) {}
-                else {
-                    configurations.style.display = "none";
-                }
-            }
-        }
-
         forEach.call(animations, function(animation) {
             toggleConfigDisplay(animation, e.target);
         });
     });
 
-
-    // to access variables and functions from the background script
-    chrome.runtime.getBackgroundPage(function (backgroundPage) {
-        // configuration dictionary that stores cfg of each animation
-        forEach.call(animations, function(animation) {
-            // for each animation, create a canvas context and scale it
-            var canvas = animation.getElementsByTagName("canvas")[0];
-            var context = canvas.getContext("2d");
-            context.scale(4, 4);
-
-            // create a new animation object and begin the animation
-            var animationName = animation.id;
-            var configurableAnimation = new backgroundPage[animationName](context);
-            configurableAnimation.initialize();
-            configurableAnimation.animate();
-
-            // save the cfg in cfg_dictionary
-            cfg_dictionary[animationName] = configurableAnimation.cfg;
-        });
-
-        /******************** SAVE CONFIGURATION PARAMETERS ********************/
-        function addAnimationToFavorites(animationType, cfg, isNew, storageID) {
-            // Create new canvas with scaled context, append to document
-            var newCanvas = document.createElement("canvas");
-            var context = newCanvas.getContext("2d");
-            newCanvas.height = 76;
-            newCanvas.width = 76;
-
-            if (isNew) {
-                chrome.storage.sync.get("animationID", function(items) {
-                    var id = items["animationID"];
-                    if (typeof id == "undefined") {
-                        id = 0;
-                    }
-                    newCanvas.id = id;
-                    chrome.storage.sync.set({animationID: id + 1});
-
-                    storeFavorite(animationType, cfg, id);
-                });
-            }
-            else {
-                newCanvas.id = storageID;
-            }
-            context.scale(4, 4);
-            document.getElementById("favorites").appendChild(newCanvas);
-
-            // Create new animation and animate it
-            var newFavoriteAnimation = new backgroundPage[animationType](context, cfg);
-            newFavoriteAnimation.initialize();
-            newFavoriteAnimation.animate();
-            newCanvas.addEventListener("click", function() {
-                chrome.runtime.sendMessage({
-                    type: "update icon",
-                    animation: animationType,
-                    cfg: cfg
-                });
-            });
-            newCanvas.addEventListener("contextmenu", function(e) {
-                e.preventDefault();
-                deleteAnimationFromFavorites(this);
-            });
-        } // End of addAnimationToFavorites()
-
-        function deleteAnimationFromFavorites(animation) {
-            chrome.storage.sync.get("allFavorites", function(items) {
-                var favoriteList = items["allFavorites"]
-                for (var i = 0; i < favoriteList.length; i++) {
-                    if (favoriteList[i].ID == animation.id) {
-                        favoriteList.splice(i, 1);
-                        chrome.storage.sync.set({allFavorites: favoriteList});
-                        break;
-                    }
-                }
-            });
-
-            animation.remove();
-        }
-
-        function storeFavorite(animationType, cfg, animationID) {
-            var newFavoriteObject = {
-                ID: animationID,
-                animation: animationType,
-                cfg: cfg
-            };
-            chrome.storage.sync.get("allFavorites", function(items) {
-                var favoriteList = items["allFavorites"];
-                if (typeof favoriteList == "undefined") {
-                    favoriteList = [];
-                }
-                favoriteList.push(newFavoriteObject);
-                chrome.storage.sync.set({allFavorites: favoriteList});
-            });
-        } // End of storeFavorite()
-
-
-        (function loadFavorites() {
-            chrome.storage.sync.get("allFavorites", function(items) {
-                forEach.call(items["allFavorites"], function(favorite) {
-                    addAnimationToFavorites(favorite.animation, favorite.cfg, false, favorite.ID);
-                });
-            });
-        })();
-
-        // When each save configuration button is clicked...
-        var saveConfigurationButtons = document.getElementsByClassName("saveConfigurations");
-        forEach.call(saveConfigurationButtons, function(button) {
-            button.addEventListener("click", function() {
-                var animationType = this.getAttribute("data-animation");
-                var cfgCopy = cloneObject(cfg_dictionary[animationType]);
-
-                // Add the configured animation to the Favorites div
-                addAnimationToFavorites(animationType, cfgCopy, true);
-            });
-        });
-
-    }); // END OF GET BACKGROUND PAGE
-
+    // When a parameter value is changed, update the cfg dictionary
     var parameters = document.getElementsByClassName("parameter");
     forEach.call(parameters, function(parameter) {
         parameter.addEventListener("click", function() {
@@ -193,6 +58,59 @@ document.addEventListener("DOMContentLoaded", function() {
             cfg_dictionary[thisAnimation][thisParameter] = this.value;
         });
     });
+
+
+    //------------------------- ACCESS BACKGROUND PAGE -------------------------
+    chrome.runtime.getBackgroundPage(function (backgroundPage) {
+        // When the popup is opened, load all available animations
+        forEach.call(animations, function(animation) {
+            // for each animation, create a canvas context and scale it
+            var canvas = animation.getElementsByTagName("canvas")[0];
+            var context = canvas.getContext("2d");
+            context.scale(4, 4);
+
+            // create a new animation object and begin the animation
+            var animationName = animation.getAttribute("data-animation");
+            var configurableAnimation = new backgroundPage[animationName](context);
+            configurableAnimation.initialize();
+            configurableAnimation.animate();
+
+            // save the cfg in cfg_dictionary
+            cfg_dictionary[animationName] = configurableAnimation.cfg;
+        });
+
+        // When the popup is opened, load all stored favorites
+        (function loadStoredFavorites() {
+            chrome.storage.sync.get("allFavorites", function(items) {
+                forEach.call(items["allFavorites"], function(favorite) {
+                    addAnimationToFavorites(favorite.animation, favorite.cfg, favorite.ID);
+                });
+            });
+        })();
+
+        // When each save configuration button is clicked...
+        var saveCfgButtons = document.getElementsByClassName("saveCfg");
+        forEach.call(saveCfgButtons, function(button) {
+            button.addEventListener("click", function() {
+                // Add the configured animation to the Favorites div
+                var animationType = this.getAttribute("data-animation");
+                var cfgCopy = cloneObject(cfg_dictionary[animationType]);
+                addAnimationToFavorites(animationType, cfgCopy);
+            });
+        });
+
+        // Add an animation to the favorites div and animate it
+        function addAnimationToFavorites(animationType, cfg, storageID) {
+            // Creates the DOM element, and saves the new favorite to storage
+            var context = addDOMCanvasToFavorites(animationType, cfg, storageID);
+
+            // Create new animation and animate it
+            var newFavoriteAnimation = new backgroundPage[animationType](context, cfg);
+            newFavoriteAnimation.initialize();
+            newFavoriteAnimation.animate();
+        }
+    });
+    //^^^^^^^^^^^^^^^^^^^^^^^ END OF GET BACKGROUND PAGE ^^^^^^^^^^^^^^^^^^^^^^^
 });
 
 /* When a jscolor is selected, send a message with its id and the color */
